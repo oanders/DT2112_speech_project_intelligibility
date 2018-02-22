586e01c7329210ce85bb3d9384328321a76c211a
Oskar Andersson <oanders@kth.se>
2018-02-22 18:09:14 +0100

    start of model, converted data to excel for easier view

---
+++ model.py
@@ -0,0 +1,13 @@
+import pandas
+import os
+
+def readData():
+    currentDir = os.getcwd()
+    path = os.path.join(currentDir, "data-extraction/formant-values.xlsx")
+    df = pandas.read_excel(path)
+    return df
+
+def main():
+    df = readData()
+    print(df)
+main()
