e7e917dd4c81c767396a1181e502780c5891dac3
Oskar Andersson <oanders@kth.se>
2018-02-26 11:22:11 +0100

    reworked model to fit new format of data

--- model.py
+++ model.py
@@ -3,52 +3,56 @@ import os
 import numpy as np
 import matplotlib as plt
 from sklearn import linear_model
+from sklearn.preprocessing import Imputer
 
 def readData():
     currentDir = os.getcwd()
-    path = os.path.join(currentDir, "data-extraction/formant-values.xlsx")
+    path = os.path.join(currentDir, "data" + os.sep +"formant-data.xlsx")
     df = pandas.read_excel(path)
     return df
 
-def getMeanValues(df):
-    f1_list = ["F1_1","F1_2","F1_3","F1_4","F1_5"]
-    f2_list = ["F2_1","F2_2","F2_3","F2_4","F2_5"]
-    df = df.assign(F1_mean=df[f1_list].mean(axis=1))
-    df = df.assign(F2_mean=df[f2_list].mean(axis=1))
-    return df
 
 #Train model to fit x -> y
 def model1(df_X, df_Y):
-    X = df_X[["F1_mean", "F2_mean"]].values
-    Y = df_Y[["F1_mean", "F2_mean"]].values
+
+    df_X_1to3 = df_X.head(86)
+    df_Y_1to3 = df_Y.head(86)
+    X = df_X_1to3[["F1", "F2"]].values
+    Y = df_Y_1to3[["F1", "F2"]].values
 
     model = linear_model.LinearRegression()
     model.fit(X, Y)
 
     print(model.coef_)
 
-def saveDFs(df_Be, df_JP, df_O):
+def saveDFs(df_B, df_J, df_O):
     writer = pandas.ExcelWriter("test.xlsx")
-    df_Be.to_excel(writer, 'Be')
-    df_JP.to_excel(writer, 'JP')
+    df_B.to_excel(writer, 'B')
+    df_J.to_excel(writer, 'J')
     df_O.to_excel(writer, 'O')
     writer.save()
 
+# Imput mean for missing F2s
+def imput(df):
+    imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    df["F2"]=imputer.fit_transform(df[["F2"]]).ravel()
+
 def main():
     df = readData()
-    df = df.replace("--undefined--", np.NaN) #Seems reasonable to just set undefined to 0 for averaging
     
-    df_Be = df[df.ID_rec.str.startswith("Be")]
-    df_JP = df[df.ID_rec.str.startswith("JP")]
-    df_O = df[df.ID_rec.str.startswith("O")]
+    # Sepparate the data sets
+    df_B = df[df['speaker'] == "B"]
+    df_J = df[df['speaker'] == "J"]
+    df_O = df[df['speaker'] == "O"]
 
-    df_Be = getMeanValues(df_Be)
-    df_JP = getMeanValues(df_JP)
-    df_O = getMeanValues(df_O)
+    # Imput missing values for F2 with mean
+    imput(df_B)
+    imput(df_J)
+    imput(df_O)
 
-    #model1(df_JP, df_O)
+    model1(df_J, df_O)
 
-    saveDFs(df_Be, df_JP, df_O)
+    saveDFs(df_B, df_J, df_O)
 
 
 main()
